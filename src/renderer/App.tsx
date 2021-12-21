b33178ed35a7fbe7bd65741e3a7b5ca8bc5d9969
Daniel Curran <danielcurran@live.co.za>
2021-12-21 17:08:45 +0200

    init :tada:

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,75 @@
+import { useRef, useState } from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
 const Hello = () => {
+  const [hasAudio, setHasAudio] = useState(true);
+  const [recordedAudio, setRecordedAudio] = useState(null);
+  const context = new AudioContext();
+  const sayButton = useRef(null);
+  const handleSuccess = (stream) => {
+    console.log(stream);
+    const options = { mimeType: 'audio/webm' };
+    const recordedChunks = [];
+    const mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.addEventListener('dataavailable', function (e) {
+      if (e.data.size > 0) recordedChunks.push(e.data);
+    });
+
+    mediaRecorder.addEventListener('stop', function () {
+      var blob = new Blob(recordedChunks, {
+        'type': 'audio/wav'
+      });
+      var reader = new FileReader();
+      reader.addEventListener("loadend", function() {
+        context.decodeAudioData(reader.result, function(buffer) {
+          setRecordedAudio(buffer);
+        },
+        function(e) {
+          console.log("error ", e)
+        });
+      });
+      reader.readAsArrayBuffer(blob);
+    });
+
+    sayButton.current.addEventListener('mouseup', () => {
+      try {
+        mediaRecorder.stop();
+      } catch (error) {
+        console.log(error);
+      }
+      stream.getTracks().forEach(function (track) {
+        track.stop();
+      });
+    });
+
+    mediaRecorder.start();
+  };
+  const onSay = () => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const audioInputs = devices.filter((d) => d.kind === 'audioinput');
+      if (audioInputs.length > 0) {
+        navigator.mediaDevices
+          .getUserMedia({
+            audio: {
+              deviceId: devices[0].deviceId,
+            },
+          })
+          .then((stream) => handleSuccess(stream))
+          .catch(error => console.log(error));
+      } else {
+        setHasAudio(false);
+      }
+    });
+  };
+  const onPlay = () => {
+    var source = context.createBufferSource();
+    source.buffer = recordedAudio;
+    source.connect(context.destination);
+    source.start(0);
+  };
   return (
     <div>
       <div className="Hello">
@@ -10,30 +77,11 @@ const Hello = () => {
       </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        {!hasAudio && <div>Shit, no audio devices detected!</div>}
+        <button onMouseDown={onSay} ref={sayButton}>
+          Say
+        </button>
+        <button type="button" onClick={onPlay}>Play</button>
       </div>
     </div>
   );
